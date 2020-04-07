0d749e46dbb43bcbbad76cb61904474212bf7c1d
srinidhishanmugam <60546230+srinidhishanmugam@users.noreply.github.com>
2020-04-07 17:22:01 +0530

    Add files via upload

---
+++ positive number list-2.py
@@ -0,0 +1,6 @@
+list2=[12,14,-95,3]
+print(list2)
+print("positive numbers:")
+for i in list2:
+    if i<0:
+        print(i,end="")
---
+++ positive numbers list-1.py
@@ -0,0 +1,7 @@
+list1=[12,-7,5,64,-14]
+print(list1)
+print("positive numbers:")
+for i in list1:
+    if i<0:
+        print(i,end="")
+    
